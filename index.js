75f85e496ea57b56b4d03fb1108175ec43381042
Khoa Pham <khoaphp@yahoo.com>
2016-11-30 10:30:08 +0700

    half

--- index.js
+++ index.js
@@ -3,16 +3,36 @@ var bodyParser = require('body-parser');
 var parser = bodyParser.urlencoded({extended: false});
 
 var multer = require('multer');
-var storage1 = multer.diskStorage({
+var storage = multer.diskStorage({
   destination: function(req, file, cb){
-    cb(null, "./public/images");
+    if(file.fieldname=="avatar"){
+      cb(null, "./public/images/hinhdaidien");
+    }else{
+      cb(null, "./public/images/hinhsanpham");
+    }
   },
   filename: function(req, file, cb){
-    cb(null, ''+Date.now()+'.png');
+    cb(null, Date.now() + file.originalname);
   }
 });
 
-var upload = multer({storage: storage1});
+function fileFilter(req, file, cb){
+  if(file.mimetype=="image/jpeg"){
+    cb(null, true);
+  }else{
+    cb(new Error("Sai dinh dang file"));
+  }
+}
+
+var limits = {
+  fileSize: 1024*20
+}
+
+// var upload = multer({storage, fileFilter, limits}).single('avatar');
+
+function getUpload(fieldname){
+  return multer({storage, fileFilter, limits}).single(fieldname);
+}
 
 var app = express();
 app.set('view engine', 'ejs');
@@ -24,7 +44,24 @@ app.get('/', function(req, res){
   res.render('home');
 });
 
-app.post('/xuly', upload.single('avatar'), function(req, res){
+app.post('/xulydangky', function(req, res){
   //TODO HERE
-  res.send(req.body.name)
+  getUpload('avatar')(req, res, function(err){
+    if(err){
+      res.send('LOI: ' + err);
+    }else{
+      res.send('Thanh cong')
+    }
+  });
+});
+
+app.post('/xulydangtin', function(req, res){
+  //TODO HERE
+  getUpload("hinhsanpham")(req, res, function(err){
+    if(err){
+      res.send(''+err);
+    }else{
+      res.send('Thanh cong');
+    }
+  });
 });
